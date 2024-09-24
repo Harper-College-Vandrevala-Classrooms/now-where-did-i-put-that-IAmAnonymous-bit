668f92bf878d47a9dbf2ece2e7013498ab461c10
NNR8608220 <n8reyes2006@gmail.com>
2024-09-24 13:46:12 -0500

    Added function with starting position argument

--- NWDIPT.cpp
+++ NWDIPT.cpp
@@ -29,6 +29,31 @@ using namespace std
     return -1;
 }
 
+;int findChar(string s, char c, int start)
+{
+    if(s.length() == 0)
+    {
+        return -1;
+    }
+
+    string sL = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+        sL += tolower(s[i]);
+    }
+
+    char cL = tolower(c);
+
+    for (int i = start; i < sL.length(); i++)
+    {
+        if(sL[i] == cL)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 ;int findString(string s, string ss)
 {
     if(ss.length() == 0 || s.length() == 0)
@@ -36,10 +61,53 @@ using namespace std
         return -1;
     }
 
-    for (int i = 0; i <= (s.length() - ss.length()); i++)
+    string sL = "";
+    string ssL = "";
+    for (int i = 0; i < s.length(); i++)
     {
-        string temp = s.substr(i,ss.length());
-        if(temp == ss)
+        sL += tolower(s[i]);
+    }
+
+    for (int i = 0; i < ss.length(); i++)
+    {
+        ssL += tolower(ss[i]);
+    }
+
+    for (int i = 0; i <= (sL.length() - ssL.length()); i++)
+    {
+        string temp = sL.substr(i,ssL.length());
+        if(temp == ssL)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+;int findString(string s, string ss, int start)
+{
+    if(ss.length() == 0 || s.length() == 0)
+    {
+        return -1;
+    }
+
+    string sL = "";
+    string ssL = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+        sL += tolower(s[i]);
+    }
+
+    for (int i = 0; i < ss.length(); i++)
+    {
+        ssL += tolower(ss[i]);
+    }
+
+    for (int i = start; i <= (sL.length() - ssL.length()); i++)
+    {
+        string temp = sL.substr(i,ssL.length());
+        if(temp == ssL)
         {
             return i;
         }
--- testNWDIPT.cpp
+++ testNWDIPT.cpp
@@ -17,6 +17,16 @@ using namespace std
     assert(findString("The quick brown fox", "ox") == 17);
 
     assert(findChar("The quick brown fox", 't') == 0);
+    assert(findChar("The quick brown fox", 'B') == 10);
+    
+    assert(findString("THE QUICK BROWN FOX", "he") == 1);
+    assert(findString("The quick brown fox", "HE") == 1);
+
+    assert(findChar("The quick brown fox", 'e', 1) == 2);
+    assert(findChar("The quick brown fox", 'e', 4) == -1);
+    
+    assert(findString("The quick brown fox", "qu", 1) == 4);
+    assert(findString("The quick brown fox", "qu", 10) == -1);
 
     assert(findString("The quick brown fox", "") == -1);
 
